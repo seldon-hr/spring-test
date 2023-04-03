945e619f97663a909a428a6e754f4f50d95e4d32
Daniel Romero <daanmaro99june@gmail.com>
2023-04-03 11:20:06 -0500

    Waiting for the response

--- src/main/resources/templates/scripts/login.js
+++ src/main/resources/templates/scripts/login.js
@@ -1,16 +1,19 @@
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
-const url = "http://localhost:8080/login/session";
-const root = url+"?user=" +username+"&pass="+password;
-
 
 function getAccess() {
+    const username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    password = String(password);
+    const url = "http://localhost:8080/login/session";
+    const root = url+"?user=" +username+"&pass="+ encodeURIComponent(password);
     debugger
+    console.log(password);
     console.log(root);
-    /* fetch(root, {
+    fetch(root, {
         mode: 'no-cors'
     })
     .then(response => response.json())
     .then(data => console.log(data))
-    .catch(error => console.error(error)); */
+    .catch(error => console.error(error)); 
 }
+
+
